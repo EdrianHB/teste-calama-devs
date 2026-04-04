73f57a2498ca8e0523218f88686e49889982c15e
Edrian <edrianboldrini.11@gmail.com>
2026-04-04 06:18:54 -0400

    feat: add button interaction

---
+++ edrian/js/script.js
@@ -0,0 +1,5 @@
+var botao = window.document.getElementsByTagName("button")[0];
+var mensagem = window.document.getElementById("mensagem");
+function AoClicar(){
+    mensagem.innerText = "Agora você faz parte do time!"
+}

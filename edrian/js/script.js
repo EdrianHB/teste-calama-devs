4b8bd9c85849f174a7c97c8412ee7048c8d41ce9
Edrian <edrianboldrini.11@gmail.com>
2026-04-05 16:58:40 -0400

    feat: text addition in js

--- edrian/js/script.js
+++ edrian/js/script.js
@@ -1,5 +1,6 @@
 var botao = window.document.getElementsByTagName("button")[0];
 var mensagem = window.document.getElementById("mensagem");
 function AoClicar(){
-    mensagem.innerText = "Agora você faz parte do time!"
+    mensagem.innerText = "Fique de olho nas próximas seleções!\nFaça seu cadastro de interesse aqui: https://forms.gle/JVNJ8RSuaXzN9aZ97\nEnquanto isso, siga e confira os projetos abertos: https://github.com/Calama-Devs"
+
 }
